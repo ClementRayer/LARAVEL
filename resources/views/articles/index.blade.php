4a6b3fff2bad57080f741b112cc19e3a3a836667
Unknown <clement.rayer@gmail.com>
2017-02-18 19:26:10 +0100

    Home and menu redirections to articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,11 +4,11 @@
 
 @foreach($articles as $article)
 
-    <center><h2>{{$article->title}}:</h2></center>
+    <center><h2><a href="/article/{{$article->id}}">{{$article->title}}:</a></h2></center>
 
     <center><p>{{$article->content}}</p></center>
 
-    <center><p> Written by: {{$article->user->name}}</p></center>
+    <center><p> Written by: <a href="/user/{{$article->user_id}}">{{$article->user->name}}</a></p></center>
 
 @endforeach
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if(Auth::check())
-                        <h2>Bonjour, {{Auth::user()->name}}</h2>
-                        <h2>Mail: {{Auth::user()->email}}</h2>
-                        <h2>Compte créé le: {{Auth::user()->created_at}}</h2>
-                        @foreach($articles as $article)
-                            <center><h3>{{$article->title}}</h3></center>
-                            <center><p>{{$article->content}}</p></center>
-                            <center><b>Written by {{$article->user->name}}</b></center>
-                        @endforeach
-                    @else
-                        <h2>Bonjour invité</h2>
-                    @endif
+                    <div class="panel-body">
+                        @if(Auth::check())
+                            <h2>Bonjour, {{Auth::user()->name}}</h2>
+                            <h3>Mail : {{Auth::user()->email}}</h3>
+                            <h3>Date d'inscription : {{Auth::user()->created_at}}</h3><br>
+                            <center><h3><a href="/user/{{Auth::user()->id}}">Mes articles</a></h3></center>
+                        @else
+                            <h2>Bonjour invité</h2>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection

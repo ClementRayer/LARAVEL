58b317c3bc8bb56ba3912fbbcc7f8452d8eba77c
Unknown <thomaspericoi@gmail.com>
2017-02-18 19:52:53 +0100

    V2

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,16 +2,27 @@
 
 @section('content')
 
-@foreach($articles as $article)
+    @foreach($articles as $article)
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
 
-    <center><h2><a href="/article/{{$article->id}}">{{$article->title}}:</a></h2></center>
+                        <center><h2><a href="/article/{{$article->id}}">{{$article->title}}:</a></h2></center>
 
-    <center><p>{{$article->content}}</p></center>
+                        <center><p>{{$article->content}}</p></center>
 
-    <center><p> Written by: <a href="/user/{{$article->user_id}}">{{$article->user->name}}</a></p></center>
+                        <center><p> Written by: <a href="/user/{{$article->user_id}}">{{$article->user->name}}</a></p>
+                        </center>
 
-@endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
 
-<center>{{$articles->links()}}</center>
+
+    @endforeach
+
+    <center>{{$articles->links()}}</center>
 
 @endsection

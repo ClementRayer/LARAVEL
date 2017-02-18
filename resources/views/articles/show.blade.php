814a46f8228b995209ff31f5385cc8801c8406b5
Unknown <thomaspericoi@gmail.com>
2017-02-18 19:50:28 +0100

    Easy navigation between pages

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.app')
 
-
 @section('content')
-    <center><h2>{{$article->title}}:</h2></center>
-    
-    <p>{{$article->content}}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h4>{{$article->title}}</h4></div>
+                    <center>
+                        <br>
+                        <p>{{$article->content}}</p><br>
+                        <b>Written by <a href="/user/{{$article->user_id}}">{{$article->user->name}}</a><br><br><br></b>
 
-    <p>Written by: {{$article->user->name}}</p>
-    
-    <ul>
-        <a href="https://www.facebook.com/sharer/sharer.php?u=http://127.0.0.1:8000/article/{{$article->id}}">
-            Partager sur Facebook
-        </a>
-        <br>
-        <a href="https://twitter.com/intent/tweet/?url=http://127.0.0.1:8000/article/{{$article->id}}&text=Regardez cet article!">
-            Partager sur Twitter
-        </a>
-    </ul>
+                        <a href="{{$article->id}}/edit">Edit</a>
+                        <br>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=http://127.0.0.1:8000/article/{{$article->id}}">Share
+                            on
+                            Facebook</a>
+                        <br>
+                        <a href="https://twitter.com/intent/tweet/?url=http://127.0.0.1:8000/article/{{$article->id}}&text=Regardez cet article!">Share
+                            on Twitter</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,8 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/article">Articles</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -61,7 +63,6 @@
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="/home">My profile</a>
-                                        <a href="/article">Articles</a>
                                         <a href="/article/create">Create an Article</a>
                                         <hr>
                                         <a href="{{ route('logout') }}"

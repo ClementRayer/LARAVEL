75f35ad3a71df43d5504b8a3ee264e1269d4a68d
Unknown <clement.rayer@gmail.com>
2017-02-20 14:17:50 +0100

    l

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -17,7 +17,7 @@ class isAdmin
     public function handle($request, Closure $next)
     {
         if(Auth::check()
-            && Auth::user()->role == "admin") {
+            && Auth::user()->role == 1) {
             return $next($request);
         }
         else{
